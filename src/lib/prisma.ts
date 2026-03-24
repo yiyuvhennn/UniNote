0b7a2a16e8dca9fb2b82aadc4aa2e5a73e4868c0
yiyuvhennn <155879114+yiyuvhennn@users.noreply.github.com>
2026-03-24 16:35:03 +0800

    init: clean fullstack uniNote project

--- src/lib/prisma.ts
+++
@@ -1,9 +0,0 @@
-import "dotenv/config";
-import { PrismaClient } from "@prisma/client";
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
-
-const adapter = new PrismaBetterSqlite3({
-  url: process.env.DATABASE_URL!,
-});
-
-export const prisma = new PrismaClient({ adapter });
